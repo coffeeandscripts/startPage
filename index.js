a42d77fa8d1f1c17b85e4ff6d52dcdd9d6055395
ersari94 <ersari94@gmail.com>
2017-03-10 11:29:23 +1100

    Preliminary submission

---
+++ index.js
@@ -0,0 +1,38 @@
+var showTime = function() {
+    setInterval(function() {
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var d = new Date();
+        var day = d.getDate();
+        if (day == 1) {
+            day = day + "st";
+        } else if (day == 2) {
+            day = day + "nd";
+        } else if (day == 3) {
+            day = day + "rd";
+        } else {
+            day = day + "th";
+        }
+        var date = days[d.getDay()] + ", " + day + " of " + months[d.getMonth()] + " " + d.getFullYear();
+        var seconds = d.getSeconds();
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        var minutes = d.getMinutes();
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        var time = d.getHours() + ":" + minutes + ":" + seconds;
+        $('#mainTitleContentDate').html(date);
+        $('#mainTitleContentTime').html(time);
+    }, 500);
+}
+
+$(document).ready(showTime);
+
+var fadeInMain = function() {
+    $('#mainPage').hide();
+    $('#mainPage').delay("800").fadeIn("slow");
+}
+
+$(document).ready(fadeInMain);
